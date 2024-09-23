3754be19e74b07d1cfd9f8aae5d6521145fd3468
Jarosław Zieliński <jaroslaw.zielinski@polcode.net>
2024-09-23 05:44:29 +0200

    Repository Extended by Search feature

--- src/Bible/Service/Offline/RepositoryInterface.php
+++ src/Bible/Service/Offline/RepositoryInterface.php
@@ -8,19 +8,29 @@ use JaroslawZielinski\Torah\Bible\Torah\AbstractSiglum;
 
 interface RepositoryInterface
 {
+    public const SEARCH_MODE_CASESENSITIVE = 1;
+
+    public const SEARCH_MODE_CASEINSENSITIVE = 2;
+
     /**
      */
     public function createTable(string $table): void;
-    
+
     /**
      */
     public function add(AbstractSiglum $siglum, string $translation, string $content): ?int;
-    
+
     /**
      * @throws \Exception
      */
     public function get(AbstractSiglum $siglum, string $translation): ?string;
-    
+
+    /**
+     * @throws \Exception
+     */
+    public function search(string $search, string $translation, int $caseSensitive = self::SEARCH_MODE_CASEINSENSITIVE)
+    : array;
+
     /**
      */
     public function totalCount(string $translation): int;
